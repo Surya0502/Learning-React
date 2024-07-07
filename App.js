884e0bb91e72ac5ae4cb6c4b71608b3914f8a7af
Surya0502 <60318888+Surya0502@users.noreply.github.com>
2024-07-07 12:18:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Hello World from React");
+// console.log(heading) // object
+const parent = React.createElement("div", { id: "parent" },
+    [React.createElement("div", { id: "child1" }, [
+        React.createElement("h1", { id: "heading" }, "Heading1"),
+        React.createElement("h2", { id: "heading" }, "Heading2")]),
+    React.createElement("div", { id: "child2" }, [
+        React.createElement("h1", { id: "heading1" }, "Heading 1"),
+        React.createElement("h2", { id: "heding 2" }, "Heading 2")
+    ])
+    ]
+)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
